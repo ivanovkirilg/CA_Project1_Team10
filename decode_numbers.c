df70b449066099b59e01a2b3ac026758b5df787d
Kiril Ivanov <ivanovkirilg@gmail.com>
2021-01-20 19:10:43 +0200

    Done

--- decode_numbers.c
+++ decode_numbers.c
@@ -3,10 +3,25 @@
 int findMSB(int iNumber);
 
 int main() {
+    int numberToDecode = 10230;
+    int mask = 0x1F;
 
+    do {
+        int currentNumber = numberToDecode & mask;
+        printf("%d\n", currentNumber);
+    } while ( numberToDecode >>= 5);
+
+    /* int iterations = 0;
+    iterations = findMSB(numberToDecode) / 5;
+    printf("msb: %d\n", findMSB(numberToDecode));
+    if (findMSB(numberToDecode) % 5) {
+        iterations++;
+    }
+    printf("%d\n", iterations); */
     
     return 0;
 }
+
 
 int findMSB(int iNumber) {
     unsigned int mask = 0x80000000;
