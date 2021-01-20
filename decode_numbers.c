f76f72bb70799637e107b261bf1b313e4dc51759
Kiril Ivanov <ivanovkirilg@gmail.com>
2021-01-20 18:20:23 +0200

    Find MSB

--- decode_numbers.c
+++ decode_numbers.c
@@ -4,3 +4,13 @@ int main() {
     
     return 0;
 }
+
+int findMSB(int iNumber) {
+    unsigned int mask = 0x80000000;
+    int bit = 0;
+    int index = 31;
+    for (bit = mask & iNumber; bit == 0 && index >= 0; index-- ){ 
+        mask >>= 1u;
+        bit = mask & iNumber;
+    }
+}
