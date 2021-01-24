b32791e0c3182d373fc76ffc46e955f94c0111d3
Kiril Ivanov <ivanovkirilg@gmail.com>
2021-01-25 00:01:43 +0200

    Formatting and UX

--- Code_Review/team12_hotel.c
+++ Code_Review/team12_hotel.c
@@ -2,11 +2,14 @@
 #include <time.h>
 #include <string.h>
 
+#define FIFTEEN_PERCENT 0.15
+#define TWENTY_PERCENT 0.20
+#define TOURIST_TAX_RATE 0.06
 #define SECONDS_TO_DAYS 24*60*60
 #define SEPARATOR "######################\n"
 
 int validDate();
-void chooseRooms(char type[], unsigned short int *number, unsigned short int *nights);
+double chooseRooms(char type[], double price, unsigned short int *number, unsigned short int *nights);
 
 int main()
 {
@@ -17,8 +20,11 @@ int main()
     unsigned short int nightsLuxury = 0;
     unsigned short int roomsStandard = 0;
     unsigned short int nightsStandard = 0;
+    unsigned short int totalNights = 0;
+    double luxuryTotal = 0;
+    double standardTotal = 0;
     double subTotal = 0;
-    float touristTax = 0;
+    double touristTax = 0;
     double total = 0;
 
     char choice = 0;
@@ -35,18 +41,17 @@ int main()
         scanf(" %c", &choice);
         printf(SEPARATOR);
 
-        switch (choice) {
+        switch (choice) 
+        {
             case '1':
-                chooseRooms("Luxury", &roomsLuxury, &nightsLuxury);
-                subTotal = roomsStandard * nightsStandard * c_standardRoomPrice;
-                subTotal += roomsLuxury * nightsLuxury * c_luxuryRoomPrice;
+                luxuryTotal = chooseRooms("Luxury", c_luxuryRoomPrice, &roomsLuxury, &nightsLuxury);
+                subTotal = luxuryTotal + standardTotal;
                 printf("Your current bill is %.2f leva.\n", subTotal);
                 break;
 
             case '2':
-                chooseRooms("Standard", &roomsStandard, &nightsStandard);
-                subTotal = roomsStandard * nightsStandard * c_standardRoomPrice;
-                subTotal += roomsLuxury * nightsLuxury * c_luxuryRoomPrice;
+                standardTotal = chooseRooms("Standard", c_standardRoomPrice, &roomsStandard, &nightsStandard);
+                subTotal = luxuryTotal + standardTotal;
                 printf("Your current bill is %.2f leva.\n", subTotal);
                 break;
 
@@ -59,47 +64,46 @@ int main()
         }
     }
 
-    unsigned short int totalNights = 0;
-    totalNights = nightsStandard + nightsLuxury;
+    totalNights = nightsStandard * roomsStandard + nightsLuxury * roomsLuxury;
 
     if (totalNights > 5 && subTotal < 700)
     {
-        printf("You get a 15 percent discount.\n");
-        double discount1 = subTotal * 0.15;
-        printf("Your discount is %.2f leva.\n", discount1);
+        double discount1 = subTotal * FIFTEEN_PERCENT;
         subTotal = subTotal - discount1;
+        printf("You get a 15 percent discount.\n");
+        printf("Your discount is %.2f leva.\n", discount1);
         printf("Your bill after discount is: %.2f leva.\n", subTotal);
-        goto finalOffer;
     }
 
     else if (subTotal > 700 && subTotal < 1000)
     {
-        printf("You can choose between a 15 percent discount and a free spa procedure for 100 leva.\n");
-        printf("For a 15 percent discount type 1.\n");
-        printf("For a free spa procedure type 2. ");
-        double discount2 = 0;
         char choice2 = 0;
-        scanf("%c", &choice2);
+        printf("You can choose between a 15 percent discount and a free spa procedure for 100 leva.\n");
+        double discount2 = 0;
 
-        while (! (choice2=='1' || choice2=='2') ) {
+        while (! (choice2=='1' || choice2=='2') ) 
+        {
+            printf("For a 15 percent discount type 1.\n");
+            printf("For a free spa procedure type 2. ");
+            scanf(" %c", &choice2);
             switch (choice2)
             {
                 case '1':
-                    printf("You get a 15 percent discount.\n");
-                    discount2 = subTotal * 0.15;
-                    printf("Your discount is %.2f leva.\n", discount2);
+                    discount2 = subTotal * FIFTEEN_PERCENT;
                     subTotal = subTotal - discount2;
+                    printf(SEPARATOR);
+                    printf("You get a 15 percent discount.\n");
+                    printf("Your discount is %.2f leva.\n", discount2);
                     printf("Your bill after discount is: %.2f leva.\n", subTotal);
-                    goto finalOffer;
                     break;
 
                 case '2':
+                    printf(SEPARATOR);
                     printf("You get a free spa procedure for 100 leva.\n");
-                    goto finalOffer;
                     break;
 
                 default:
-                    printf("Invalid choice!\n");
+                    printf("Invalid choice! ");
                     break;
             }
         }
@@ -107,67 +111,66 @@ int main()
 
     else if (subTotal > 1000)
     {
-        printf("You can choose between a 20 percent discount, a free spa procedure for 100 leva and 2 free dinners for your family.\n");
-        printf("For a 20 percent discount type 1.\n");
-        printf("For a free spa procedure type 2.\n");
-        printf("For 2 free dinners for your family type 3. ");
-        double discount3 = 0;
         char choice3 = 0;
-        scanf("%c", &choice3);
+        printf("You can choose between a 20 percent discount,\n a free spa procedure for 100 leva and\n 2 free dinners for your family.\n");
+        double discount3 = 0;
 
-        while (! (choice3=='1' || choice3=='2' || choice3=='3') ) {
+        while (! (choice3=='1' || choice3=='2' || choice3=='3') ) 
+        {
+            printf("For a 20 percent discount type 1.\n");
+            printf("For a free spa procedure type 2.\n");
+            printf("For 2 free dinners for your family type 3. ");
+            scanf(" %c", &choice3);
             switch (choice3)
             {
                 case '1':
-                    printf("You get a 20 percent discount.\n");
-                    discount3 = subTotal * 0.20;
-                    printf("Your discount is %.2f leva.\n", discount3);
+                    discount3 = subTotal * TWENTY_PERCENT;
                     subTotal = subTotal - discount3;
+                    printf(SEPARATOR);
+                    printf("You get a 20 percent discount.\n");
+                    printf("Your discount is %.2f leva.\n", discount3);
                     printf("Your bill after discount is: %.2f leva.\n", subTotal);
-                    goto finalOffer;
                     break;
 
                 case '2':
+                    printf(SEPARATOR);
                     printf("You get a free spa procedure for 100 leva.\n");
-                    goto finalOffer;
                     break;
 
                 case '3':
+                    printf(SEPARATOR);
                     printf("You get 2 free dinners for your family.\n");
-                    goto finalOffer;
                     break;
                     
                 default:
-                    printf("Invalid choice!\n");
+                    printf("Invalid choice! ");
                     break;
             }
         }
     }
 
-    finalOffer:
-        touristTax = subTotal * 0.06;
-        printf("You owe tourist tax: %.2f leva.\n", touristTax);
-        total = subTotal + touristTax;
-        printf(SEPARATOR);
-        printf("Your final offer is:\n");
-        printf("Standard rooms: %d\n", roomsStandard);
-        printf("Luxury rooms: %d\n", roomsLuxury);
-        printf("Total nights: %d\n", totalNights);
-        printf("Final bill: %.2f leva\n", total);
-        printf(SEPARATOR);
-        validDate();
-        printf(SEPARATOR);
+    touristTax = subTotal * TOURIST_TAX_RATE;
+    total = subTotal + touristTax;
+    printf("You owe tourist tax: %.2f leva.\n", touristTax);
+    printf("Your final offer is:\n");
+    printf("%d Standard rooms for %d nights.\n", roomsStandard, nightsStandard);
+    printf("%d Luxury rooms for %d nights.\n", roomsLuxury, nightsLuxury);
+    printf("Total nights: %d\n", totalNights);
+    printf("Final bill: %.2f leva\n", total);
+    validDate();
+    printf(SEPARATOR);
 
    return 0;
 }
 
-void chooseRooms(char type[], unsigned short int *number, unsigned short int *nights) {
+double chooseRooms(char type[], double price, unsigned short int *number, unsigned short int *nights) {
     /* Вече повторното избиране на стая презаписва избора, а не добавя към предишния */
     printf("Your choice is %s room.\n", type);
     printf("How many nights are you going to stay? ");
     scanf("%hu", nights);
     printf("How many %s rooms would you like? ", type);
     scanf("%hu", number);
+    return price * *number * *nights;
 }
 
 int validDate(){
