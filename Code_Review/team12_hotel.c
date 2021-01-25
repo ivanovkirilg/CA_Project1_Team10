e0cf200d231c4a8b4610b115d751a82f1c0a0e6f
Kiril Ivanov <ivanovkirilg@gmail.com>
2021-01-25 10:32:34 +0200

    Discounts into function

--- Code_Review/team12_hotel.c
+++ Code_Review/team12_hotel.c
@@ -8,8 +8,9 @@
 #define SECONDS_TO_DAYS 24*60*60
 #define SEPARATOR "######################\n"
 
-int validDate();
 double chooseRooms(char type[], double price, unsigned short int *number, unsigned short int *nights);
+double discountsAndBonuses(unsigned short int nights, double subTotal);
+int validDate();
 
 int main()
 {
@@ -24,6 +25,7 @@ int main()
     double luxuryTotal = 0;
     double standardTotal = 0;
     double subTotal = 0;
+    double discount = 0;
     double touristTax = 0;
     double total = 0;
 
@@ -69,90 +71,14 @@ int main()
     /* ------------ ОТСТЪПКИ И БОНУСИ ------------  */
     totalNights = nightsStandard * roomsStandard + nightsLuxury * roomsLuxury;
 
-    if (totalNights > 5 && subTotal < 700)
+    discount = discountsAndBonuses(totalNights, subTotal);
+    if ((int) discount) 
     {
-        double discount1 = subTotal * FIFTEEN_PERCENT;
-        subTotal = subTotal - discount1;
-        printf("You get a 15 percent discount.\n");
-        printf("Your discount is %.2f leva.\n", discount1);
+        subTotal -= discount;
+        printf("Your discount is %.2f leva.\n", discount);
         printf("Your bill after discount is: %.2f leva.\n", subTotal);
     }
-
-    /* Отстъпка за над 700лв оборот */
-    else if (subTotal > 700 && subTotal < 1000)
-    {
-        char choice2 = 0;
-        printf("You can choose between a 15 percent discount and a free spa procedure for 100 leva.\n");
-        double discount2 = 0;
-
-        while (! (choice2=='1' || choice2=='2') ) 
-        {
-            printf("For a 15 percent discount type 1.\n");
-            printf("For a free spa procedure type 2. ");
-            scanf(" %c", &choice2);
-            switch (choice2)
-            {
-                case '1':
-                    discount2 = subTotal * FIFTEEN_PERCENT;
-                    subTotal = subTotal - discount2;
-                    printf(SEPARATOR);
-                    printf("You get a 15 percent discount.\n");
-                    printf("Your discount is %.2f leva.\n", discount2);
-                    printf("Your bill after discount is: %.2f leva.\n", subTotal);
-                    break;
-
-                case '2':
-                    printf(SEPARATOR);
-                    printf("You get a free spa procedure for 100 leva.\n");
-                    break;
-
-                default:
-                    printf("Invalid choice! ");
-                    break;
-            }
-        }
-    }
-
-    /* Отстъпка за над 1000лв оборот */
-    else if (subTotal > 1000)
-    {
-        char choice3 = 0;
-        printf("You can choose between a 20 percent discount,\n a free spa procedure for 100 leva and\n 2 free dinners for your family.\n");
-        double discount3 = 0;
-
-        while (! (choice3=='1' || choice3=='2' || choice3=='3') ) 
-        {
-            printf("For a 20 percent discount type 1.\n");
-            printf("For a free spa procedure type 2.\n");
-            printf("For 2 free dinners for your family type 3. ");
-            scanf(" %c", &choice3);
-            switch (choice3)
-            {
-                case '1':
-                    discount3 = subTotal * TWENTY_PERCENT;
-                    subTotal = subTotal - discount3;
-                    printf(SEPARATOR);
-                    printf("You get a 20 percent discount.\n");
-                    printf("Your discount is %.2f leva.\n", discount3);
-                    printf("Your bill after discount is: %.2f leva.\n", subTotal);
-                    break;
-
-                case '2':
-                    printf(SEPARATOR);
-                    printf("You get a free spa procedure for 100 leva.\n");
-                    break;
-
-                case '3':
-                    printf(SEPARATOR);
-                    printf("You get 2 free dinners for your family.\n");
-                    break;
-                    
-                default:
-                    printf("Invalid choice! ");
-                    break;
-            }
-        }
-    }
+    
 
     /* ------------ ФИНАЛНА ОФЕРТА ------------ */
     touristTax = subTotal * TOURIST_TAX_RATE;
@@ -169,7 +95,8 @@ int main()
    return 0;
 }
 
-double chooseRooms(char type[], double price, unsigned short int *number, unsigned short int *nights) {
+double chooseRooms(char type[], double price, unsigned short int *number, unsigned short int *nights) 
+{
     /* Вече повторното избиране на стая презаписва избора, а не добавя към предишния */
     printf("Your choice is %s room.\n", type);
     printf("How many nights are you going to stay? ");
@@ -179,7 +106,87 @@ double chooseRooms(char type[], double price, unsigned short int *number, unsign
     return price * *number * *nights;
 }
 
-int validDate(){
+double discountsAndBonuses(unsigned short int nights, double subTotal)
+{
+    double discount = 0;
+    char choice = 0;
+
+    if (nights > 5 && subTotal < 700)
+    {
+        discount = subTotal * FIFTEEN_PERCENT;
+        printf("You get a 15 percent discount.\n");
+        return discount;
+    }
+
+    /* Отстъпка за над 700лв оборот */
+    else if (subTotal >= 700 && subTotal < 1000)
+    {
+        printf("You can choose between a 15 percent discount and a free spa procedure for 100 leva.\n");
+
+        while (! (choice=='1' || choice=='2') ) 
+        {
+            printf("For a 15 percent discount type 1.\n");
+            printf("For a free spa procedure type 2. ");
+            scanf(" %c", &choice);
+            switch (choice)
+            {
+                case '1':
+                    discount = subTotal * FIFTEEN_PERCENT;
+                    printf(SEPARATOR);
+                    printf("You get a 15 percent discount.\n");
+                    return discount;
+
+                case '2':
+                    printf(SEPARATOR);
+                    printf("You get a free spa procedure for 100 leva.\n");
+                    return 0.0;
+
+                default:
+                    printf("Invalid choice!\n");
+                    break;
+            }
+        }
+    }
+
+    /* Отстъпка за над 1000лв оборот */
+    else if (subTotal >= 1000)
+    {
+        printf("You can choose between a 20 percent discount,\n a free spa procedure for 100 leva and\n 2 free dinners for your family.\n");
+
+        while (! (choice=='1' || choice=='2' || choice=='3') ) 
+        {
+            printf("For a 20 percent discount type 1.\n");
+            printf("For a free spa procedure type 2.\n");
+            printf("For 2 free dinners for your family type 3. ");
+            scanf(" %c", &choice);
+            switch (choice)
+            {
+                case '1':
+                    discount = subTotal * TWENTY_PERCENT;
+                    printf(SEPARATOR);
+                    printf("You get a 20 percent discount.\n");
+                    return discount;
+
+                case '2':
+                    printf(SEPARATOR);
+                    printf("You get a free spa procedure for 100 leva.\n");
+                    return 0.0;
+
+                case '3':
+                    printf(SEPARATOR);
+                    printf("You get 2 free dinners for your family.\n");
+                    return 0.0;
+                    
+                default:
+                    printf("Invalid choice!\n");
+                    break;
+            }
+        }
+    }
+}
+
+int validDate()
+{
     long long int t;
     t = time(NULL);
     t += 14 * SECONDS_TO_DAYS; /* две седмици (14 дена) след сегашното време */
